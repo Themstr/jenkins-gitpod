1726837099da0db34ad01d06b32f2ab956f13343
traumatized hemstr <109982611+Themstr@users.noreply.github.com>
2023-01-03 21:50:22 +0100

    switching to openjdk

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -1,12 +1,6 @@
 commands=(
-    # Adding Eclipse jdk Key
-    "wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -"
-    
     # Adding Jenkins Key
     "wget -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -"
-
-    # Adding Eclipse Jdk repo 
-    "echo \"deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main\" | sudo tee /etc/apt/sources.list.d/adoptium.list"
 
     # Adding Jenkins repo
     "echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list"
@@ -14,8 +8,8 @@ commands=(
     # Syncing repos
     "sudo apt update"
 
-    # Installing jenkins / jdk
-    "sudo apt install temurin-11-jdk jenkins -y"
+    # Installing jenkins / jre
+    "sudo apt install default-jre jenkins -y"
 
     # Disable Docker Permission Denied
     "sudo chmod 666 /var/run/docker.sock"
