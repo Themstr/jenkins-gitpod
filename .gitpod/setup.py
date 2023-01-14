284ec1b045f2a9eac37693c03afdec6c436bb897
traumatized hemstr <109982611+Themstr@users.noreply.github.com>
2023-01-14 14:05:54 +0000

    adding kuber

--- .gitpod/setup.py
+++ .gitpod/setup.py
@@ -55,4 +55,52 @@ tasks.append(
         """sudo service jenkins start"""
     ]
 )
+tasks.append(
+    [
+        "Installing VirtualBox",
+        """sudo apt-get install -y virtualbox virtualbox-ext-pack"""
+    ]
+)
+tasks.append(
+    [
+        "adding Kubernetes keys",
+        """curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"""
+    ]
+)
+tasks.append(
+    [
+        "apt file kuber",
+        """sudo touch /etc/apt/sources.list.d/kubernetes.list"""
+    ]
+)
+tasks.appned(
+    [
+        "Adding Kuber",
+        """echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list"""
+    ]
+)
+tasks.appned(
+    [
+        "Updating",
+        """sudo apt update"""
+    ]
+)
+tasks.append(
+    [
+        "Installing kuber",
+        """sudo apt-get install -y kubectl"""
+    ]
+)
+tasks.append(
+    [
+        "Installing Minikube",
+        """curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64"""
+    ]
+)
+tasks.append(
+    [
+        "Adding minikube to path",
+        """chmod +x minikube && sudo mv minikube /usr/local/bin/"""
+    ]
+)
 runTasks(tasks)
